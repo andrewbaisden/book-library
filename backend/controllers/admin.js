3103ab5204c66c6c48bffc7733ac44bfc9465665
Andrew Baisden <andrewbaisden@gmail.com>
2020-08-12 15:06:14 +0100

    backend REST API completed

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -1,3 +1,35 @@
+const Books = require('../models/Books');
+
 exports.getIndex = (req, res) => {
 	res.send('Book Library Home Route');
+};
+
+exports.getBooks = (req, res) => {
+	Books.fetchAll((books) => {
+		console.log(books);
+		res.json(books);
+	});
+};
+
+exports.getBook = (req, res) => {
+	const bookId = req.params.bookId;
+
+	Books.findById(bookId, (book) => {
+		console.log(book);
+		res.json(book);
+	});
+};
+
+exports.postAddBook = (req, res) => {
+	const name = req.body.name;
+	const author = req.body.author;
+	const narrated = req.body.narrated;
+	const img = req.body.img;
+	const bookLength = req.body.bookLength;
+	const releaseDate = req.body.releaseDate;
+	const language = req.body.language;
+
+	const book = new Books(null, name, author, narrated, img, bookLength, releaseDate, language);
+	book.save();
+	res.json({ msg: 'Book Added' });
 };
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -4,4 +4,10 @@ const adminController = require('../controllers/admin');
 
 router.get('/', adminController.getIndex);
 
+router.get('/books', adminController.getBooks);
+
+router.get('/books/:bookId', adminController.getBook);
+
+router.post('/add-book', adminController.postAddBook);
+
 module.exports = router;
